6119789168b7619a894d97ea77567557e041929a
Tony E Ford <tlcagford@gmail.com>
2025-11-30 07:59:05 -0700

    Initialize theoretical_correction module imports

---
+++ theoretical_correction/__init__.py
@@ -0,0 +1,8 @@
+from .entanglement_density_models import (
+    EntanglementDensityBase, 
+    EarlyDarkEnergyLikeModel,
+    PersistentEntanglementModel, 
+    QuantumCoherenceModel,
+    get_entanglement_model
+)
+from .modified_friedmann_solver import ModifiedFriedmannSolver
